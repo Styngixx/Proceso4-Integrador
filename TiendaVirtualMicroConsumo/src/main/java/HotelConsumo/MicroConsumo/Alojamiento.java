4e3c367eaf8d20ddb6337f434adf47b7e208c5a7
Gabriell <gabrielmartinezasalde01@gmail.com>
2026-06-12 13:06:20 -0500

    NUEVOPROCESO4

---
+++ TiendaVirtualMicroConsumo/src/main/java/HotelConsumo/MicroConsumo/Alojamiento.java
@@ -0,0 +1,18 @@
+package HotelConsumo.MicroConsumo;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Alojamiento {
+    private Long idAlojamiento;
+    private String numeroHabitacion;
+    private String tipoHabitacion;
+
+    public Alojamiento() {}
+
+    public Long getIdAlojamiento() { return idAlojamiento; }
+    public void setIdAlojamiento(Long idAlojamiento) { this.idAlojamiento = idAlojamiento; }
+    public String getNumeroHabitacion() { return numeroHabitacion; }
+    public void setNumeroHabitacion(String numeroHabitacion) { this.numeroHabitacion = numeroHabitacion; }
+    public String getTipoHabitacion() { return tipoHabitacion; }
+    public void setTipoHabitacion(String tipoHabitacion) { this.tipoHabitacion = tipoHabitacion; }
+}
--- TiendaVirtualMicroConsumo/src/main/java/HotelConsumo/MicroConsumo/FichaConsumo.java
+++ TiendaVirtualMicroConsumo/src/main/java/HotelConsumo/MicroConsumo/FichaConsumo.java
@@ -6,11 +6,20 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@JsonPropertyOrder({ "id", "empleado", "carrito", "servicios", "importeTotal", "fechaHora" })
+@JsonPropertyOrder({ "id", "empleado", "alojamiento", "carrito", "servicios", "importeTotal", "fechaHora" })
 public class FichaConsumo {
-    @Id private Long id;
-    @Embedded private Empleado empleado;
-    @Embedded private Carrito carrito;
+    
+    @Id 
+    private Long id;
+    
+    @Embedded 
+    private Empleado empleado;
+
+    @Embedded 
+    private Alojamiento alojamiento; // <-- NUEVO BLOQUE
+
+    @Embedded 
+    private Carrito carrito;
     
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "ficha_id")
@@ -20,17 +29,26 @@ public class FichaConsumo {
     private LocalDateTime fechaHora;
 
     public FichaConsumo() {}
-    // Getters y Setters...
+
+    // Getters y Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+    
     public Empleado getEmpleado() { return empleado; }
     public void setEmpleado(Empleado empleado) { this.empleado = empleado; }
-    public List<ServicioAdquirido> getServicios() { return servicios; }
-    public void setServicios(List<ServicioAdquirido> servicios) { this.servicios = servicios; }
+    
+    public Alojamiento getAlojamiento() { return alojamiento; } // <-- NUEVO GETTER
+    public void setAlojamiento(Alojamiento alojamiento) { this.alojamiento = alojamiento; } // <-- NUEVO SETTER
+
     public Carrito getCarrito() { return carrito; }
     public void setCarrito(Carrito carrito) { this.carrito = carrito; }
+    
+    public List<ServicioAdquirido> getServicios() { return servicios; }
+    public void setServicios(List<ServicioAdquirido> servicios) { this.servicios = servicios; }
+    
     public double getImporteTotal() { return importeTotal; }
     public void setImporteTotal(double importeTotal) { this.importeTotal = importeTotal; }
+    
     public LocalDateTime getFechaHora() { return fechaHora; }
     public void setFechaHora(LocalDateTime fechaHora) { this.fechaHora = fechaHora; }
 }
